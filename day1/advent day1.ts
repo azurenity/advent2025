f0bbe32210036b4342596bd259617e37b1241f8a
Phua Guan Cen <gc@Phuas-MacBook-Air.local>
2025-12-04 21:30:21 +0800

    added advent day1 inputs

---
+++ day1/advent day1.ts
@@ -0,0 +1,15 @@
+// my input is basically a whole ass line of code where 
+// there will be L/R
+// then there will be an accompanying number which represents the magnitude
+
+// first, we need to be able to split it into single instructions
+// then my function should be able to process said instructions
+
+// function should take in the instruction, 
+// check the direction that it needs to shift by
+// shift the dial by said amount,
+// check if the dial is pointing the absolute 0
+// increment 1 each time its at the absolute 0
+function shiftDial(string:string) {
+    
+}
